c9498c91d4914ee4741ebe38c84cca37817cfae3
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-12-20 21:03:11 +0530

    Added table schema

--- backend/db/tables.py
+++ backend/db/tables.py
@@ -1,7 +1,15 @@
 '''Creating Raw log data table'''
+import os
+
+from python_path import PythonPath
 
 from google.cloud import bigquery #pylint: disable=E0401
 from google.cloud.exceptions import NotFound
+
+path = os.path.join("..", "..")
+
+with PythonPath(path, relative_to = __file__):
+    from backend.common.dbt_logger import logger
 
 def create_table(client, table_name, dataset_name):
     """Create table if not exist"""
@@ -13,17 +21,51 @@ def create_table(client, table_name, dataset_name):
 
     try:
         table = client.get_table(table_ref)
-        print(f'table {table} already exists.')
+        logger.info('table %s already exists.', table)
     except NotFound:
         schema = [
             bigquery.SchemaField(
-                'api_request_number', 
-                'INTEGER', 
+                'date',
+                'TIMESTAMP',
                 mode='REQUIRED',
-                description='Test description'
+                description='Date when API was used'
+            ),
+            bigquery.SchemaField(
+                'ip_address', 
+                'STRING', 
+                mode='REQUIRED',
+                description='Fake ip address of user'
+            ),
+            bigquery.SchemaField(
+                'user_agent',
+                'STRING',
+                mode='REQUIRED',
+                description='Used to identify identify the application, operating system of user'
+            ),
+            bigquery.SchemaField(
+                'request_type',
+                'STRING',
+                mode='REQUIRED',
+                description='API request type'
+            ),
+            bigquery.SchemaField(
+                'status_code',
+                'STRING',
+                mode='REQUIRED',
+                description='Statuscode of API request'
+            ),
+            bigquery.SchemaField(
+                'api_req_no', 
+                'STRING', 
+                description='API request number'
+            ),
+            bigquery.SchemaField(
+                'username', 
+                'STRING', 
+                description='Username'
             )
         ]
         table = bigquery.Table(table_ref, schema=schema)
+        table.partitioning_type = "DAY"
         table = client.create_table(table)
-        print(f'table {table.table_id} created.')
-    return table
+        logger.info('table %s created.', table.table_id)
