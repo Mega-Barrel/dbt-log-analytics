e44bfac9cb7caa6f7efff3e1cc6a733312a928a6
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-12-20 21:02:27 +0530

    BQ client

--- backend/db/bq_client.py
+++ backend/db/bq_client.py
@@ -2,13 +2,18 @@
 
 import os
 from google.cloud import bigquery #pylint: disable=E0401
+from google.oauth2 import service_account
 
 def client_oauth(file_path):
     """Authenticate BigQuery Service Account credentials"""
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = file_path
 
+    credentials = service_account.Credentials.from_service_account_file(file_path)
+
+    # Extract the project ID from the credentials
+    project_id = credentials.project_id
     # Create client object
-    client = bigquery.Client()
+    client = bigquery.Client(project=project_id)
 
     # return obj
     return client
